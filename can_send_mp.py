748aa5da65d5c7338b6a34eb2aaa3dd3b43316cd
Schaeffkoch <Schaeffkoch@d19c66e1-1f4b-2163-b0e7-93c1df8af052>
2012-08-26 17:51:36 +0000

    +chaned multiprocessing example

--- can_send_mp.py
+++ can_send_mp.py
@@ -9,33 +9,36 @@ import time
 import msvcrt
 
 
-def send(data):
+def send(data, id, cycle):
     can=canlib_xl.can_api()
     ok=can.channel_init()
     
     n=0
     while (n < 100):
-        print data
-        can.send_msg(data[0], data[1])
-        time.sleep(0.01)
+        print id
+        can.send_msg(data, id)
+        time.sleep(cycle)
         n+=1
     can.channel_close()
     return
 
 
-max_process=10
+max_process=5
 
 
 def main():
 
 #    p=multiprocessing.Process(target=send, args=([0x1, 0x2, 0x3], (0x100) ) )
 #    p.start()
-    args=list()
-    p=multiprocessing.Pool(processes=max_process)
+
+    jobs=list()
     for n in range(0, max_process):
-        args.append(([0x1, 0x2, 0x3], (0x100+n)))
+        p=multiprocessing.Process(target=send, args=([0x1, 0x2, 0x3], 0x100+n, 0.01*n))
+        jobs.append(p)
+        p.start()
+    print jobs
     #print args
-    p.map(send, args)
+
 
     
 
